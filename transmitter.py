193df75114d4f70be8c09271eec35ce7a91708ec
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-15 15:17:21 +0200

    Extended transmitter

--- transmitter.py
+++ transmitter.py
@@ -9,3 +9,21 @@ def getTextToTransmit():
 
 def encodeText():
 	return np.unpackbits(np.fromstring(getTextToTransmit(), dtype="uint8"))
+
+def waveformFormer( encodedText ):
+
+	w = []
+
+	# TODO: How do we want to transmite our characters ?
+
+def writeChannelOutput():
+	encodedText = encodeText()
+	output = waveformFormer(encodedText)
+
+	with open("files/bytes_to_transmit.txt", "w") as f:
+		f.write(output)
+
+
+writeChannelOutput()
+
+
