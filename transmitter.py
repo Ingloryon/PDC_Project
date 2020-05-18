d46fe32764e78940b3872d68dfa994997c440a3a
guillaumeP <guillaume.parchet@epfl.ch>
2020-05-18 19:16:33 +0200

    Add documentation to the methods

--- transmitter.py
+++ transmitter.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Gets the text to be transmitted from the corresponding file and return it as a string
 def getTextToTransmit():
 
 	with open("files/text_to_transmit.txt", "r") as f:
@@ -7,9 +8,12 @@ def getTextToTransmit():
 
 	return text_to_transmit;
 
-def encodeText():
-	return np.unpackbits(np.fromstring(getTextToTransmit(), dtype="uint8"))
-
+# Takes a string as input and returns an array of its UTF-8 binary encoding
+def encodeText( text ):
+	#each character is a vector of 8 bits
+	return np.unpackbits(np.fromstring(text, dtype="uint8"))
+	
+# Takes an array of binary UTF-8 characters, returns ??
 def waveformFormer( encodedText ):
 
 	w = []
@@ -19,14 +23,13 @@ def waveformFormer( encodedText ):
 	#-> nb of samples per bit
 	#-> Best techniques to counter AWGN effect
 
-def writeChannelOutput():
-	encodedText = encodeText()
-	output = waveformFormer(encodedText)
-
+# Takes a string and writes it in the file corresponding to the channel input
+def writeChannelInput( output ):
 	with open("files/channel_input.txt", "w") as f:
 		f.write(output)
 
-
-writeChannelOutput()
+		
+encodedText = encodeText( getTextToTransmit() )
+writeChannelInput( waveformFormer(encodedText) )
 
 
