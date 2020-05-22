3ea9392d11db509dd76ae9ceae80bd533f3fc19a
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-22 20:15:20 +0200

    Add linux commands to send info to PDC server

--- sendAndReceive.sh
+++ sendAndReceive.sh
@@ -2,11 +2,7 @@
 
 python transmitter.py
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[channel_input.txt]}" )" >/dev/null 2>&1 && pwd )/files/client.py"
-
-python3 $DIR --input_file files/$1 --output_file files/channel_output.txt --srv hostname=iscsrv72.epfl.ch --srv port=80
-
-/home/guillaume/Desktop/Cours/PDC_project/Repo/PDC_Project>py client.py --input_file=files/channel_input.txt --output_file=files/channel_output.txt --srv_hostname=iscsrv72.epfl.ch –srv_port=80
+python3 /home/guillaume/Desktop/Cours/PDC_project/Repo/PDC_Project/client.py --input_file files/channel_input.txt --output_file files/channel_output.txt --srv_hostname iscsrv72.epfl.ch --srv_port 80
 
 python receiver.py
 
