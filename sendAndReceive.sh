bffc6fcc4823fce17fe8cb67179c4e1c043da5ac
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-25 19:35:16 +0200

    Use python3 instead of python

--- sendAndReceive.sh
+++ sendAndReceive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python transmitter.py
+python3 transmitter.py
 
 python3 /home/guillaume/Desktop/Cours/PDC_project/Repo/PDC_Project/client.py --input_file files/channel_input.txt --output_file files/channel_output.txt --srv_hostname iscsrv72.epfl.ch --srv_port 80
 
-python receiver.py
+python3 receiver.py
 
 
 
